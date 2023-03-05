c353820267cc992c38fc1775cdab4f1748cb7443
Aayush Gehlot <aayushgehlot11@gmail.com>
2023-03-05 13:42:29 +0530

    Lowest common ancestor

---
+++ BT/236-lowestCommonAncestorBT.py
@@ -0,0 +1,36 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root == p or root == q:
+            return root
+        left = right = None
+
+        if root.left:
+            left = self.lowestCommonAncestor(root.left, p, q)
+        if root.right:
+            right = self.lowestCommonAncestor(root.right, p, q)
+
+        if left and right:
+            return root
+        else:
+            return left or right
+
+
+if __name__ == '__main__':
+    root = TreeNode(3)
+    root.left = TreeNode(5)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(6)
+    root.left.right = TreeNode(2)
+    root.right.left = TreeNode(0)
+    root.right.right = TreeNode(8)
+    p = 5
+    q = 1
+    a = Solution()
+    print(a.lowestCommonAncestor(root, p, q))
