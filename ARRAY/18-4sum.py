4c455e23a29ea2e111a59482c33fbce72c69b464
Aayush Gehlot <aayushgehlot11@gmail.com>
2023-03-28 15:22:43 +0530

    brute force

---
+++ ARRAY/18-4sum.py
@@ -0,0 +1,28 @@
+class Solution:
+    def fourSum(self, nums, target: int):
+        nums.sort()
+        n = len(nums)
+        ans = set()
+
+        for i in range(n):
+            for j in range(i + 1, n):
+                l, r = j + 1, n - 1
+
+                remain = target - nums[i] - nums[j]
+                while l < r:
+                    if nums[l] + nums[r] == remain:
+                        ans.add((nums[i], nums[j], nums[l], nums[r]))
+                        l += 1
+                        r -= 1
+                    elif nums[l] + nums[r] > remain:
+                        r -= 1
+                    else:
+                        l += 1
+        return ans
+
+
+if __name__ == '__main__':
+    nums = [1, 0, -1, 0, -2, 2]
+    target = 0
+    a = Solution()
+    print(a.fourSum(nums, target))
