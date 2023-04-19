af88fdee772d163a2ca780f3e82b4f2692439886
Aayush Gehlot <aayushgehlot11@gmail.com>
2023-04-19 07:30:17 +0530

    use stack

---
+++ STACK & QUEUE/503-nextGreaterElement2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def nextGreaterElements(self, nums):
+        stack, res = [], [-1] * len(nums)
+        # this loop will run in cyclic form
+        for i in list(range(len(nums))) * 2:
+            while stack and nums[stack[-1]] < nums[i]:
+                res[stack.pop()] = nums[i]
+            stack.append(i)
+        return res
+
+
+if __name__ == '__main__':
+    nums = [1, 2, 3, 4, 3]
+    a = Solution()
+    print(a.nextGreaterElements(nums))
