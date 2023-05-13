3865f57b068bed7add479ca7ed2e7d8508c4362f
Aayush Gehlot <aayushgehlot11@gmail.com>
2023-05-13 11:47:25 +0530

    use bfs

---
+++ BT/1161-maxlevelSum.py
@@ -0,0 +1,39 @@
+import collections
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def maxLevelSum(self, root) -> int:
+        maxSum, level, maxLevel = -float('inf'), 0, 0
+        q = collections.deque()
+        q.append(root)
+        while q:
+            level += 1
+            sum = 0
+            for _ in range(len(q)):
+                node = q.popleft()
+                sum += node.val
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            if maxSum < sum:
+                maxSum, maxLevel = sum, level
+        return maxLevel
+
+
+if __name__ == '__main__':
+    root = TreeNode(1)
+    root.left = TreeNode(7)
+    root.right = TreeNode(0)
+    root.left.left = TreeNode(7)
+    root.left.right = TreeNode(-8)
+
+    a = Solution()
+    print(a.maxLevelSum(root))
